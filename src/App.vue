be440a9d9fa9c99a428171d0e15ce3ebdeb544c6
OlegBirykov <p_petro75@mail.ru>
2022-02-05 19:41:16 +0300

    Добавлены компоненты из проекта-прототипа

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div>
+  <div class="todo-app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Короче, добро пожаловать"/>
+    <h3>Задания: {{toDoses.length}}</h3>
+    <TodoInput :addTodoItem="addTodoItem"></TodoInput>
+    <TodoList
+      v-if="toDoses.length > 0"
+      :TodoListItems="toDoses"
+      :DoneUndone="doneUndoneItem">
+    </TodoList>
+    <p v-else>Добавьте первое задание</p>
   </div>
 </template>
 
+<style lang="scss" src="./App.scss"></style>
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import TodoList from './components/TodoList/TodoList.vue'
+    import TodoInput from './components/TodoInput/TodoInput.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        data() {
+            return {
+                toDoses: [],
+                addTodoItem: (value) => {
+                    if (value.length > 0) {
+                        let data = new Date();
+                        this.toDoses.push({
+                            id: data.getTime(),
+                            name: value,
+                            done: false,
+                        })
+                    }
+                },
+                doneUndoneItem: (idx, donex) => {
+                    const el = this.toDoses.findIndex((el) => el.id === idx);
+
+                    this.toDoses = this.toDoses.map(({id, name, done}) => {
+                        if (idx === id) {
+                            return {
+                                id,
+                                name,
+                                done: donex
+                            }
+                        } else {
+                            return {
+                                id,
+                                name,
+                                done
+
+                            }
+                        }
+                    });
+                },
+
+            }
+        },
+        watch: {
+            toDoses: (val, oldVal) => {
+                console.log(val, oldVal);
+                window.localStorage.setItem('localToDoses', JSON.stringify(val))
+            }
+        },
+        mounted: function () {
+            let local = window.localStorage.getItem('localToDoses');
+            console.log(this.toDoses);
+            if (JSON.parse(local)) {
+                this.toDoses = JSON.parse(local)
+            }
+        },
+        components: {
+            TodoList,
+            TodoInput
+        },
+        created() {
+        }
+    }
+
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TodoList/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="todo-list">
+    <TodoListItem
+      v-for="(item, i) in listItems"
+      :key="i"
+      :TodoListItem="item"
+      :DoneUndoneX="DoneUndone">
+      {{item}}
+    </TodoListItem>
+  </ul>
+</template>
+
+<style lang="scss" src="./TodoList.scss"></style>
+
+<script>
+    import TodoListItem from './TodoListItem/TodoListItem.vue';
+
+    export default {
+        name: 'TodoList',
+        props: {
+            TodoListItems: Array,
+            DoneUndone: Function,
+            DeleteItem: Function
+        },
+        data() {
+            return {
+                listItems: this.TodoListItems,
+                DoneUndone_: this.DoneUndone
+            }
+
+        },
+        created() {
+        },
+        components: {
+            TodoListItem
+        }
+    }
+</script>
